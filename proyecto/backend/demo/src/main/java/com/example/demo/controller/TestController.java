d2ade52dd507c6fadec6d64e7457eb4acd932db0
ValenciaJ56 <juavalenciaga@unal.edu.co>
2025-11-16 16:48:11 -0500

    conexion correcta
    conexion correcta entre el backend y el frontend

--- proyecto/backend/demo/src/main/java/com/example/demo/controller/TestController.java
+++ proyecto/backend/demo/src/main/java/com/example/demo/controller/TestController.java
@@ -1,11 +1,14 @@
 package com.example.demo.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping ("/api/prueba")
 public class TestController {
-    @GetMapping("/hola")
+
+    @GetMapping
     public String hola() {
         return "Hola desde el backend!";
     }
--- proyecto/frontend/src/pages/Prueba.jsx
+++ proyecto/frontend/src/pages/Prueba.jsx
@@ -1,7 +1,27 @@
-function Prueba(){
-    return (
-        <h1 style={{color: "blue"}}>Juan es feo</h1>
-    )
+import { useEffect, useState } from "react";
+
+function Prueba() {
+  const [mensaje, setMensaje] = useState("");
+
+  useEffect(() => {
+    const cargarMensaje = async () => {
+      try {
+        const res = await fetch("http://localhost:8080/api/prueba", {
+          method: "GET",
+        });
+        const texto = await res.text(); // porque tu backend devuelve un String
+        setMensaje(texto);
+      } catch (err) {
+        console.error(err);
+      }
+    };
+
+    cargarMensaje();
+  }, []);
+
+  return (
+    <h1 style={{ color: "blue" }}>{mensaje}</h1>
+  );
 }
 
-export default Prueba
+export default Prueba;
