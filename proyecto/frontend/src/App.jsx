7b5e28d163aecea420241d061f5c54a12314f133
ValenciaJ56 <juavalenciaga@unal.edu.co>
2025-11-17 18:56:42 -0500

    cambio

--- proyecto/frontend/src/App.jsx
+++ proyecto/frontend/src/App.jsx
@@ -1,11 +1,13 @@
 import Inicio from "./pages/Inicio"
-import { BrowserRouter, Routes, Route } from "react-router-dom"
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom"
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Inicio />} />
+        <Route path="/Chef" element={<Inicio />} />
+        <Route path="/Despachador" element={<Inicio />} />
       </Routes>
     </BrowserRouter>
   )
