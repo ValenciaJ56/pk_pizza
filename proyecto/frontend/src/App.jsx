5c48931a569f644256408f1bff73c62d71d098ec
ValenciaJ56 <juavalenciaga@unal.edu.co>
2025-11-17 20:58:58 -0500

    error 404 funcion

--- proyecto/frontend/src/App.jsx
+++ proyecto/frontend/src/App.jsx
@@ -1,6 +1,7 @@
 import Inicio from "./pages/Inicio"
 import Chef from "./pages/Chef"
 import Despachador from "./pages/Despachador"
+import Error from "./pages/Error"
 
 import Navbar from "./components/Navbar"
 import { BrowserRouter, Routes, Route} from "react-router-dom"
@@ -12,6 +13,8 @@ function App() {
         <Route path="/" element={<Inicio />} />
         <Route path="/Chef" element={<Chef />} />
         <Route path="/Despachador" element={<Despachador />} />
+
+        <Route path="*" element={<Error />} />
 
         <Route path="/Prueba" element={<Navbar />} /> 
       </Routes>
