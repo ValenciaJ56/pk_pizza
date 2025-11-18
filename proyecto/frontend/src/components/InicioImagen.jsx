59c7847d39a2702d003fb23e920f103b23605dba
Sebasmesacalle <semesac@unal.edu.co>
2025-11-17 19:35:48 -0500

    Manito

--- proyecto/frontend/src/components/InicioImagen.jsx
+++ proyecto/frontend/src/components/InicioImagen.jsx
@@ -2,8 +2,8 @@ import fondogrande from "../assets/fondogrande.jpg"
 
 const Imagen_inicio = () => {
   return (
-    <div className="contenedor-imagen h-96 bg-gray-300 flex justify-center items-center">
-      <img src={fondogrande} alt="Imagen de inicio" />
+    <div className="contenedorimageninicio w-full">
+      <img src={fondogrande} alt="Imagen de inicio" className="" />
     </div>
   )
 }
