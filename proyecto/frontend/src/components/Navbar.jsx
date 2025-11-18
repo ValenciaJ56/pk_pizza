c2ecf068e78774b3353cf29d03ccca234476bb54
ValenciaJ56 <juavalenciaga@unal.edu.co>
2025-11-17 22:47:13 -0500

    buena

--- proyecto/frontend/src/components/Navbar.jsx
+++ proyecto/frontend/src/components/Navbar.jsx
@@ -1,12 +1,6 @@
 import { useNavigate } from "react-router-dom"
 
 const Navbar = () => {
-  const navegar = useNavigate();
-  
-  const irAInicio = () => {
-    navegar("/");
-  }
-
   return (
     //Pagina para una pizzeria
     <h1 className="bg-[#c41e3a] p-2"></h1>
