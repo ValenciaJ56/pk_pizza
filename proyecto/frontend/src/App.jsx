e5a72f391f9a06a7340bf6312d1d1273a8df9ce6
ValenciaJ56 <juavalenciaga@unal.edu.co>
2025-11-17 19:59:37 -0500

    cambio

--- proyecto/frontend/src/App.jsx
+++ proyecto/frontend/src/App.jsx
@@ -1,18 +1,13 @@
 import Inicio from "./pages/Inicio"
 import Navbar from "./components/Navbar"
-<<<<<<< Updated upstream
-import { BrowserRouter, Routes, Route} from "react-router-dom"
-=======
 import { BrowserRouter, Routes, Route, Link } from "react-router-dom"
 import PizzeriaPanel from './pages/PizzeriaPanel'
->>>>>>> Stashed changes
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Inicio />} />
-        <Route path="/Chef" element={<Inicio />} />
         <Route path="/Despachador" element={<Inicio />} />
         <Route path="/Prueba" element={<Navbar />} />
         <Route path="/Chef" element={<PizzeriaPanel />} />
