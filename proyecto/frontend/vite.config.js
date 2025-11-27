b44a2908143f9b83993e011158a381ed7c028a50
ValenciaJ56 <juavalenciaga@unal.edu.co>
2025-11-26 21:24:17 -0500

    cambios

--- proyecto/frontend/vite.config.js
+++ proyecto/frontend/vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/pk_pizza/',
+  //base: '/pk_pizza/',
   plugins: [react()],
 });
